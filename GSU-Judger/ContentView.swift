d117ab10a0e9a327105e8ca805e56c1e8dbd7541
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2023-10-17 11:19:28 -0400

    Initial Commit

---
+++ GSU-Judger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GSU-Judger
+//
+//  Created by Christopher Yoon on 10/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GSU-Judger/GSU_JudgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GSU_JudgerApp.swift
+//  GSU-Judger
+//
+//  Created by Christopher Yoon on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct GSU_JudgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
