873cf308c941f8a896d5f8285bdf491aaaec12d1
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2023-11-15 16:55:44 -0500

    Removed extra old comments

--- GSU-Judger/GSU_JudgerApp.swift
+++ GSU-Judger/GSU_JudgerApp.swift
@@ -8,19 +8,6 @@
 import SwiftUI
 import Firebase
 
-//import FirebaseCore
-//import FirebaseAuth
-
-//class AppDelegate: NSObject, UIApplicationDelegate {
-//    func application(_ application: UIApplication,
-//                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-//        FirebaseApp.configure()
-//        return true
-//    }
-//}
-
-
-
 @main
 struct GSU_JudgerApp: App {
 //    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
